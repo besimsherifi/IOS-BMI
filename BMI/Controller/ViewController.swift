eece5d7ec9719d11e07ef3e60e1c6dd4ce4dc3df
besimsherifi <sherifibesim@gmail.com>
2022-04-22 15:41:37 +0200

    added some functionality to the sliders

--- BMI/Controller/ViewController.swift
+++ BMI/Controller/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var height: UILabel!
+    @IBOutlet weak var weight: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    var bmi = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightChanged(_ sender: UISlider) {
+        let heightLabel = String(format: "%.2f",(sender.value))
+        height.text = "\(heightLabel) m"
+    }
+    
+    @IBAction func weightChanged(_ sender: UISlider) {
+        let weightLabel = String(format: "%.0f",(sender.value))
+        weight.text = "\(weightLabel) KG"
+    }
+    
+    @IBAction func calculate(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        bmi = Int(weight / (height * height))
+        print(bmi)
+    }
+    
 }
 
